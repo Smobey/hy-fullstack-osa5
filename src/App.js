3f79cdad0d24b3aec64b45ee62eac52d404c1e19
Topi T <smobey@gmail.com>
2018-03-03 17:02:56 +0200

    5.3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ class App extends React.Component {
       error: null,
       username: '',
       password: '',
-      user: null
+      user: null,
+      newTitle: '',
+      newAuthor: '',
+      newUrl: ''
     }
   }
 
@@ -24,8 +27,26 @@ class App extends React.Component {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       this.setState({user})
+      blogService.setToken(user.token)
     }
   } 
+
+  addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: this.state.newTitle,
+      author: this.state.newAuthor,
+      url: this.state.newUrl
+    }
+
+    blogService
+      .create(blogObject)
+      .then(newBlog => {
+        this.setState({
+          username: ''
+        })
+      })
+  }
 
   login = async (event) => {
     event.preventDefault()
@@ -37,11 +58,12 @@ class App extends React.Component {
       })
 
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       this.setState({ username: '', password: '', user})
 
     } catch(exception) {
       this.setState({
-        error: 'käyttäjätunnus tai salasana virheellinen',
+        error: 'invalid username or password',
       })
       setTimeout(() => {
         this.setState({ error: null })
@@ -49,7 +71,7 @@ class App extends React.Component {
     }
   }
 
-  handleLoginFieldChange = (event) => {
+  handleFieldChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
 
@@ -65,24 +87,14 @@ class App extends React.Component {
       <h2>Kirjaudu sovellukseen</h2>
       <form onSubmit={this.login}>
         <div>
-          käyttäjätunnus
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleLoginFieldChange}
-          />
+          username
+          <input type="text" name="username" value={this.state.username} onChange={this.handleFieldChange}/>
         </div>
         <div>
-          salasana
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleLoginFieldChange}
-          />
+          password
+          <input type="password" name="password" value={this.state.password} onChange={this.handleFieldChange}/>
         </div>
-        <button type="submit">kirjaudu</button>
+        <button type="submit">log in</button>
       </form>
     </div>
   )
@@ -91,6 +103,23 @@ class App extends React.Component {
     <div>
         <p>{this.state.user.name} logged in</p>
         <button type="submit" onClick={this.logout}>kirjaudu ulos</button>
+
+        <h2>create new</h2>
+        <form onSubmit={this.addBlog}>
+        <div>
+          title
+          <input type="text" name="newTitle" value={this.state.newTitle} onChange={this.handleFieldChange}/>
+        </div>
+        <div>
+          author
+          <input type="text" name="newAuthor" value={this.state.newAuthor} onChange={this.handleFieldChange}/>
+        </div>
+        <div>
+          url
+          <input type="text" name="newUrl" value={this.state.newUrl} onChange={this.handleFieldChange}/>
+        </div>
+        <button type="submit">create</button>
+      </form>
 
         <h2>blogs</h2>
         {this.state.blogs.map(blog => 
